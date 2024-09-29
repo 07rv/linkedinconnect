5de362c222260c18b41074cf7d8c8ffcae92b4bb
Rohit Verma <28vrohit@gmail.com>
2024-09-29 14:40:46 +0530

    add outh

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from oauth import oauth2_scheme, get_current_user, create_access_token
+from schemas import User,Login
 
-from peopleConnect import Connect
+users_db = {
+    "user1": User(username="user1", password="password1"),
+    "user2": User(username="user2", password="password2"),
+}
 
-Connect().PeopleConnect()
+app = FastAPI()
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = users_db.get(form_data.username)
+    if user is None or user.password != form_data.password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/protected")
+async def protected_route(username: str = Depends(get_current_user)):
+    return {"message": f"Hello, {username}! This is a protected resource."}
