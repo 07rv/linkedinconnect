e6c959102bf44ddb2d23039d4465e52f3f7afd3a
Rohit Verma <28vrohit@gmail.com>
2024-09-29 21:02:46 +0530

    .

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from oauth import get_current_user, create_access_token
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from companyConnect import Connect as company
+from peopleConnect import Connect as people
 
+company().CompanyConnect()
 
-app = FastAPI()
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    try:
-        USERNAME = os.getenv('USER_NAME_LOGIN')
-        USERPASSWORD = os.getenv('USER_PASSWORD_LOGIN')
-        
-        if USERNAME != form_data.username or USERPASSWORD != form_data.password:
-            raise HTTPException(status_code=400, detail="Incorrect username or password")
-        
-        access_token = create_access_token(data={'data': {"user": USERNAME}})
-        return {"access_token": access_token, "token_type": "bearer"}
-    except Exception as e:
-        print(e)
-
-@app.get("/protected")
-async def protected_route(username: str = Depends(get_current_user)):
-    return {"message": f"Hello, {username}! This is a protected resource."}
+people().PeopleConnect()
